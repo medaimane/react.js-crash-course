3d6549fc0a54d9d9a25518c791c7d43cdaed4c72
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2019-04-06 23:16:39 +0200

    feat: clean up/create first class based component

---
+++ src/components/app/App.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import './App.css';
+import Products from '../products/Products';
+
+class App extends Component {
+    state = {
+        products: [{
+            id: 1,
+            name: 'Product A',
+            description: 'Product A description',
+            price: '92 €',
+            review: '5',
+            isAvailable: true
+        }, {
+            id: 2,
+            name: 'Product B',
+            description: 'Product B description',
+            price: '30 €',
+            review: '4',
+            isAvailable: true
+        }, {
+            id: 3,
+            name: 'Product C',
+            description: 'Product C description',
+            price: '50 €',
+            review: '4',
+            isAvailable: true
+        }, {
+            id: 4,
+            name: 'Product D',
+            description: 'Product D description',
+            price: '73 €',
+            review: '2',
+            isAvailable: true
+        }, {
+            id: 5,
+            name: 'Product E',
+            description: 'Product E description',
+            price: '49 €',
+            review: '3',
+            isAvailable: true
+        }]
+    };
+
+    render() {
+        return (
+            <main>
+                <header>
+                    <h1>Welcome to the store</h1>
+                </header>
+                <div>
+                    <h2>Products list</h2>
+                    <Products products={this.state.products}/>
+                </div>
+            </main>
+        );
+    }
+}
+
+export default App;
