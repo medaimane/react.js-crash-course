10bf0101225d7597bd57befb45dfc58aac1f43a9
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2019-04-07 00:12:11 +0200

    feat: add basic react styling

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,7 @@ class App extends Component {
     state = {
         products: [{
             id: 1,
+            imageUrl: '',
             name: 'Product A',
             description: 'Product A description',
             price: '92 €',
@@ -12,6 +13,7 @@ class App extends Component {
             isAvailable: true
         }, {
             id: 2,
+            imageUrl: '',
             name: 'Product B',
             description: 'Product B description',
             price: '30 €',
@@ -19,13 +21,15 @@ class App extends Component {
             isAvailable: true
         }, {
             id: 3,
+            imageUrl: '',
             name: 'Product C',
             description: 'Product C description',
             price: '50 €',
             review: '4',
-            isAvailable: true
+            isAvailable: false
         }, {
             id: 4,
+            imageUrl: '',
             name: 'Product D',
             description: 'Product D description',
             price: '73 €',
@@ -33,11 +37,12 @@ class App extends Component {
             isAvailable: true
         }, {
             id: 5,
+            imageUrl: '',
             name: 'Product E',
             description: 'Product E description',
             price: '49 €',
             review: '3',
-            isAvailable: true
+            isAvailable: false
         }]
     };
 
--- src/components/product-item/ProductItem.js
+++ src/components/product-item/ProductItem.js
@@ -2,16 +2,54 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class ProductItem extends Component {
+
+    /**
+     * Using dynamic styling.
+     */
+    getAvailableStyle(isAvailable) {
+        return {
+            color:isAvailable ? 'green' : 'red'
+        };
+    }
+
     render() {
         const product = this.props.product;
         return (
-            <div className="ProductCard">
-                <h3 className="ProductName">{this.props.product.name}</h3>
-                <p>{product.description}</p>
-                <div>
+            /**
+             * Add Some inline Styling
+             */
+            <div style={{
+                border: '2px solid gray',
+                margin: '5px',
+                padding: '5px'
+            }}>
+                <h3 style={{
+                    padding: '3px',
+                    fontFamily: 'sans-serif',
+                    fontSize: '20px',
+                    textDecoration: 'none',
+                    textAlign: 'center',
+                    backgroundImage: product.imageUrl
+                }}>{this.props.product.name}</h3>
+                <p style={styles.description}>{product.description}</p>
+                <div style={styles.details}>
                     <p>Price: {product.price}</p>
                     <p>Review: {product.review}</p>
-                    <p>{product.isAvailable ? 'Available' : 'Not Available'}</p>
+                    <p style={this.getAvailableStyle(product.isAvailable)}>{product.isAvailable ? 'Available' : 'Not Available'}</p>
+                </div>
+                <div style={{
+                    margin: '4px'
+                }}>
+                    <button style={{
+                        backgroundColor: 'blue',
+                        color: 'white',
+                        margin: '2px'
+                    }}>Add to Card</button>
+                    <button style={{
+                        backgroundColor: 'orange',
+                        color: 'blue',
+                        margin: '2px'
+                    }}>Buy Now</button>
                 </div>
             </div>
         )
@@ -20,6 +58,19 @@ class ProductItem extends Component {
 
 ProductItem.propTypes = {
     product: PropTypes.object.isRequired
+};
+
+/**
+ * Add variable for styling
+ */
+const styles = {
+    description: {
+        color: 'gray',
+        padding: '3px',
+    },
+    details: {
+        margin: '5px'
+    }
 };
 
 export default ProductItem;
